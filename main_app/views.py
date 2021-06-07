a05f4df01300af07a51c69bae5b2c1d196a138b8
pmiller5254 <pmiller5254@bths.edu>
2021-06-07 12:25:27 -0400

    error

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
 
+# auth imports
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+# import models
+from .models import Player, Game, Record
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
--- speedracer/urls.py
+++ speedracer/urls.py
@@ -17,6 +17,8 @@ import main_app
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
